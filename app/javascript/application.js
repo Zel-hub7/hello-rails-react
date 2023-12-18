bc527c592025c3fb94fe93a3cc942769523bc007
unknown <zelalemyohannes75@gmail.com>
2023-12-18 14:30:23 +0300

    add react component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from "react"
+import ReactDOM from 'react-dom';
+
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <h1>Hello react</h1>
+);
