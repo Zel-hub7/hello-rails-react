a4b104737d0c2ee0c976c9f78d45fa54f3e2fc4b
unknown <zelalemyohannes75@gmail.com>
2023-12-18 20:19:03 +0300

    add redux toolkits

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,8 +3,14 @@ import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<App/>
+ReactDOM.render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
