009c0212c519f113c3a7b41d7673a25b0a7a04c6
unknown <zelalemyohannes75@gmail.com>
2023-12-18 21:23:26 +0300

    add store

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
     <BrowserRouter>
--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import greetingsReducer from "./greetingsSlice";
 
 
+
 const store = configureStore({
     reducer: {
       greetings: greetingsReducer,
